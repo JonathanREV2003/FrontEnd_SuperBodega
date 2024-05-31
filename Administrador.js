9500145e153cb8ac6913d993bf769c048b193f63
kevincord <85372037+kevincord@users.noreply.github.com>
2024-05-30 23:43:40 -0600

    Agregué ventana Administrador

---
+++ Administrador.js
@@ -0,0 +1,15 @@
+document.getElementById('btnProductos').addEventListener('click', function() {
+    alert('Productos');
+});
+
+document.getElementById('btnClientes').addEventListener('click', function() {
+    alert('Clientes');
+});
+
+document.getElementById('btnProveedores').addEventListener('click', function() {
+    alert('Proveedores');
+});
+
+document.getElementById('btnCompra').addEventListener('click', function() {
+    alert('Compra');
+});
